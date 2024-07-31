bfc2f25eb01642597e017b8d8e666324238a5223
TecStylos <tecstylos@gmail.com>
2024-07-31 11:32:02 +0200

    Added default values to Position

--- libQrawlr/include/Position.h
+++ libQrawlr/include/Position.h
@@ -7,10 +7,10 @@ namespace qrawlr
 {
     struct Position
     {
-        int tree_id;
-        int index;
-        int line;
-        int column;
+        int tree_id = -1;
+        int index = -1;
+        int line = -1;
+        int column = -1;
 
         std::string to_string() const;
         std::string to_string(std::function<std::string(int)> tree_id_to_name) const;
